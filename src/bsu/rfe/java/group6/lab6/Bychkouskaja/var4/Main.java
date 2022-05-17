129a5159422591752f500dda2c077225844f4edc
BychkovskajaPolina <polina14ght@mail.ru>
2022-05-18 00:07:51 +0300

    created main

--- src/bsu/rfe/java/group6/lab6/Bychkouskaja/var4/Main.java
+++ src/bsu/rfe/java/group6/lab6/Bychkouskaja/var4/Main.java
@@ -1,8 +1,13 @@
 package bsu.rfe.java.group6.lab6.Bychkouskaja.var4;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+// Создать и сделать видимым главное окно приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
